0a7b4b8845b65bf0b91232f8a31e0b3e5ba0d8d7
Roland C. Dowdeswell <elric@imrryr.org>
2021-02-05 11:15:43 +0000

    <mod><shift>p for dom0, <mod>p for vms.
    After all, its more unusual to need to use dom0 programs after
    you are finished setting up.

--- config.def.h
+++ config.def.h
@@ -90,8 +90,8 @@ static const char *dmenu_vms[] = { "dmenu_run", "vms", "-a", "-m", dmenumon,
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,               XK_p,      spawn,          {.v = dmenu_dom0}},
-	{ MODKEY|ShiftMask,     XK_p,      spawn,          {.v = dmenu_vms}},
+	{ MODKEY,               XK_p,      spawn,          {.v = dmenu_vms}},
+	{ MODKEY|ShiftMask,     XK_p,      spawn,          {.v = dmenu_dom0}},
 	{ MODKEY|ShiftMask,     XK_Return, spawn,          {.v = termcmd}},
 	{ MODKEY,               XK_b,      togglebar,      {0} },
 	{ MODKEY,               XK_j,      focusstack,     {.i = +1 } },
